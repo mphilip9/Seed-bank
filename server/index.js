1332d83d064deecc13ca949cc75d8c6d394c62cb
Maxwell Philip <max.philip1@gmail.com>
2022-11-17 10:05:39 -0500

    change port

--- server/index.js
+++ server/index.js
@@ -3,24 +3,23 @@ const axios = require("axios");
 const path = require('path');
 const controllers = require('../database/controllers');
 
-const PORT = process.env.NODE_ENV || 3001;
+const PORT = process.env.PORT || 3001;
 
 
 
 const app = express();
-if (process.env.NODE_ENV === 'staging' || process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve(__dirname, '../client/build')));
-}
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
 
 app.get("/api", controllers.searchPlants);
 
 app.get("/api/users", controllers.getUserData)
 
-if (process.env.NODE_ENV === 'staging' || process.env.NODE_ENV === 'production') {
+
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
   });
-}
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
