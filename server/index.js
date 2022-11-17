1b7b946c0a97dcf198fd9051b0888e94fecc5644
Maxwell Philip <max.philip1@gmail.com>
2022-11-17 09:33:39 -0500

    deploy again

--- server/index.js
+++ server/index.js
@@ -5,12 +5,22 @@ const controllers = require('../database/controllers');
 
 const PORT = process.env.PORT || 3001;
 
-const app = express();
 
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+const app = express();
+if (process.env.NODE_ENV === 'staging' || process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve(__dirname, '../client/build')));
+}
+
 app.get("/api", controllers.searchPlants);
 
 app.get("/api/users", controllers.getUserData)
+
+if (process.env.NODE_ENV === 'staging' || process.env.NODE_ENV === 'production') {
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
